c2ef38da38b31234ecc1881f4d7bb797e89c92af
ropaillet <ropaillet@gmail.com>
2017-07-09 21:25:56 -0300

    musify pagina 1 ready

--- Spotify Clone/spoticlone.js
+++ Spotify Clone/spoticlone.js
@@ -17,7 +17,6 @@ $("section").on("click",".fa-star-o",function(){
 	$(this).toggle()
 	$(fullStar).toggleClass("hidden")
 	pushToStorage($(this).attr("id"))
-
 })
 
 // FUNCTIONS //
@@ -28,6 +27,7 @@ function getArtist(artistVal){
       dataType: 'json',
       success: function(data){
       	if(data.artists.items.length > 0){
+      		queriedArtists = []
       		var cantidadArtistas = data.artists.items.length < 5 ? data.artists.items.length : 5; 
       		showData(data,cantidadArtistas)	
       	}else{
@@ -58,11 +58,17 @@ function showData(dataArtista,cantidadArtistas){
 			name :dataArtista.artists.items[i].name,
 			img : diskImg
 		}		
-		queriedArtists = []
 		queriedArtists.push(artistObject)
+		var isFav = checkIfFav(artistObject)
+		if (isFav){
+			var defaultStar = "<i class='fa fa-star' id='"+dataArtista.artists.items[i].id+"Full'></i><i class='fa fa-star-o hidden' id='" + dataArtista.artists.items[i].id +"'></i>"
+		}
+		if (!isFav) {
+			var defaultStar = "<i class='fa fa-star hidden' id='"+dataArtista.artists.items[i].id+"Full'></i><i class='fa fa-star-o' id='" + dataArtista.artists.items[i].id +"'></i>"
+		}
 		$(".resultsArea").append('<div class="col-sm-2">'+
 					'<img class="img-circle album" src="' + diskImg +'">'+
-					"<h4 class='albumName'><i class='fa fa-star hidden' id='"+dataArtista.artists.items[i].id+"Full'></i><i class='fa fa-star-o' id='" + dataArtista.artists.items[i].id +"'></i><span class='artistName'>"+ " " + dataArtista.artists.items[i].name+
+					"<h4 class='albumName'>"+defaultStar+"<span class='artistName'>"+ " " + dataArtista.artists.items[i].name+
 					"</span></h4></div>"
 		)
 	}
@@ -78,14 +84,37 @@ function pushToStorage(idClicked){
 			localStorage.setItem("latestFav", JSON.stringify(queriedArtists[i]));
 			existingFavs.push(queriedArtists[i])
 			localStorage.setItem("favs", JSON.stringify(existingFavs));
+		return
 		}
 	}	
+}
+
+// cuando traigo los artistas, esta funcion chequea  si estan presentes en  storage y les muestra estrella llena if y
+
+function checkIfFav (artistObject) {
+	var currentFavs = getFavs()
+	if (!currentFavs){
+		return	false
+	}
+	if(currentFavs){
+		for (var i = 0 ; i < currentFavs.length ; i++) {
+			if (currentFavs[i].id == artistObject.id) {
+				return true
+			}
+		}
+	return	false 
+	}
 }
 
 // Traer los favoritos y hacer el append a mi area
 function getFavs(){
     var existingFavs = JSON.parse(localStorage.getItem("favs"));
-    console.log(existingFavs)
+    if (existingFavs){
+    	return existingFavs
+    } if (!existingFavs){
+    	return false
+    }
 }
 
 
+////////////////////////////////////// Solapa Favoritos //////////////////////////////////////
