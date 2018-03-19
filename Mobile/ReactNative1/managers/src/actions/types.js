75ea31024a56f2b81388d750487c508bfcfee285
ropaillet <ropaillet@gmail.com>
2018-03-19 16:08:43 -0300

    rarara
    sadad

--- Mobile/ReactNative1/managers/src/actions/types.js
+++
@@ -1,6 +0,0 @@
-export const EMAIL_CHANGED = 'email_changed';
-export const PWD_CHANGED = 'pwd_changed';
-export const LOGIN_SUCCESS = 'login_success';
-export const LOGIN_USER_FAILED = 'login_user_failed';
-export const LOGIN_PROCESSING = 'login_processing';
-export const LOGOUT_USER = 'logout_user';
--- Mobile/ReactNative1/managers/src/components/common/index.js
+++
@@ -1,6 +0,0 @@
-export * from './Input';
-export * from './Spinner1';
-export * from './Button1';
-export * from './CardSection';
-export * from './AppHeader';
-export * from './Card';
